18d982d2c3fcd9a9fd5e73247cc5ad16a122117e
ckarren <ckarren@ncsu.edu>
2023-10-23 12:48:11 -0400

    change names

---
+++ compare_linear_hydraulic.py
@@ -0,0 +1,72 @@
+import numpy as np 
+from lin_model import * 
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+cont_demand = set_demand_pattern()
+tank_constant = calc_constant()
+initial_height = 3.0
+n_demand_states = 7
+n_tank_states = 14
+
+tank_states = create_states(0.0,6.5,n_tank_states)
+print(tank_states)
+
+i = disc_pump_rules(6.3, n_tank_states)
+j = disc_pump_rules(4.4, n_tank_states)
+k = disc_pump_rules(4.0, n_tank_states)
+l = disc_pump_rules(1.0, n_tank_states)
+
+
+round_disc_demand = round_to_disc(cont_demand,n_demand_states)
+print(round_disc_demand[2])
+
+demand_state_val = np.unique(round_disc_demand[1])
+phd = deltaHeight(demand_state_val, tank_states, tank_constant)
+#  print(phd)
+
+mt_linear = lin_tank_height(cont_demand, initial_height)        # linear model 
+mt_disc = disc_tank_height(round_disc_demand, n_tank_states, initial_height)    #discretized model
+gt = epanet_groundtruth(initial_height)     #hydraulic sim in WNTR
+mt_dict = dict_tank_height(round_disc_demand, phd, n_tank_states, h_init=initial_height)        #dictionary model 
+
+#  for i in range(169):
+    #  print('linear:', mt_linear[2][i], mt_linear[1][i], '| disc:', mt_disc[2][i], mt_disc[1][i], '| dict:', mt_dict[2][i], mt_dict[1][i],  '| epanet:', gt[1][i], gt[0][i])
+    # print(i, 'disc:', mt_disc[1][i], mt_disc[2][i], 'dict', mt_dict[1][i], mt_dict[2][i])
+
+x = mt_linear[0]
+lin_y = mt_linear[1]
+disc_y = mt_disc[1]
+gt_y = gt[0]
+dict_y = mt_dict[1]
+gt_p = gt[1]
+lin_p = mt_linear[2]
+dis_p = mt_disc[2]
+
+# for i, item in enumerate(gt_p):
+#     print("groundtruth:", round(gt_y[i],2), gt_p[i], "linear:", round(lin_y[i],2), lin_p[i], "discrete:", disc_y[i], dis_p[i])
+    # if item != dis_p[i]:
+    #     print(i)
+    # else:
+    #     pass
+r = calc_rmse(disc_y, gt_y)
+r_label = "RMSE=" + str(r)
+plt.rcParams['axes.spines.right']=False
+plt.rcParams['axes.spines.top']=False
+plt.rcParams.update({'font.size':20})
+fig, ax = plt.subplots()
+# ax.plot(x, lin_y, 'tab:orange', label='Linear model')
+ax.plot(x, gt_y, 'tab:blue', linewidth=3.0, label='EPANET simulation')
+ax.plot(x, disc_y, 'tab:orange', linewidth=3.0, label='Linear-discrete model')
+#  ax.plot(x, dict_y, 'tab:red', label='Dictionary model')
+ax.set(xlabel='Time (hr)', ylabel='TANK water level (m)')
+at = AnchoredText(
+    r_label, prop=dict(size=16), frameon=False, loc='lower right')
+#  at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+ax.add_artist(at)
+ax.legend(frameon=False)
+plt.show()
+
+# fig.savefig()
---
+++ linear_regression.py
@@ -0,0 +1,57 @@
+import wntr
+import wntr.network.controls as controls
+from sklearn import linear_model 
+import warnings
+from lin_model import *
+warnings.simplefilter("ignore", UserWarning)
+
+# tank_height, flow, demand
+regime1 = [[],[]]
+regime2 = [[],[]]
+
+minitown = wntr.network.WaterNetworkModel('minitown_map03.inp')
+minitown.options.time.duration = 604800
+def set_demand_pattern(demand_file = 'demand.txt'):
+    demand_pat = np.loadtxt(demand_file)
+    return demand_pat
+demand = set_demand_pattern()
+
+sim = wntr.sim.EpanetSimulator(minitown)
+results = sim.run_sim()
+tank_height = results.node['pressure'].loc[:,'TANK']
+p1_stat = results.link['status'].loc[:, 'PUMP1']     
+p2_stat = results.link['status'].loc[:,'PUMP2']
+tank_flow = results.node['demand'].loc[:,'TANK'] * 1000
+p1_flow = results.link['flowrate'].loc[:,'PUMP1'] * 1000 
+p2_flow = results.link['flowrate'].loc[:,'PUMP2'] * 1000
+
+def regress(network='minitown'):
+    if network == 'minitown':
+        for t, status in enumerate(p2_stat):
+            if status == 1: 
+                total_flow = p1_flow.loc[t*3600] + p2_flow.loc[t*3600]
+                regime2[0].append([tank_height.loc[t*3600],demand[t]])
+                regime2[1].append(total_flow)
+                # regime2[2].append(demand[t])
+            elif status == 0:
+                total_flow = p1_flow.loc[t*3600]
+                regime1[0].append([tank_height.loc[t*3600],demand[t]])
+                regime1[1].append(total_flow)
+                # regime1[2].append(demand[t])
+
+        xmat1 = regime1[0]
+        y1 = regime1[1]
+
+        xmat2 = regime2[0]
+        y2 = regime2[1]
+
+        reg1 = linear_model.LinearRegression()
+        reg2 = linear_model.LinearRegression()
+
+        reg1.fit(xmat1, y1)
+        reg2.fit(xmat2,  y2)
+
+        eq1 = (reg1.intercept_, reg1.coef_[0], reg1.coef_[1])
+        eq2 = (reg2.intercept_, reg2.coef_[0], reg2.coef_[1])
+
+    return eq1, eq2 
