64d1adb1c557b1879a126d64872f68a08d7bd2a9
ckarren <ckarren@ncsu.edu>
2024-03-18 10:17:29 -0400

    graphics

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,8 @@ import wntr
 import numpy as np
 #  import wntr.network.controls as controls
 from sklearn import linear_model 
+import matplotlib.pyplot as plt 
+from mpl_toolkits.mplot3d import Axes3D
 import warnings
 warnings.simplefilter("ignore", UserWarning)
 
@@ -36,22 +38,62 @@ def regress(network='minitown'):
                 regime1[0].append([tank_height.loc[t*3600],demand[t]])
                 regime1[1].append(total_flow)
 
-        xmat1 = regime1[0]
-        y1 = regime1[1]
+        X1 = regime1[0]
+        Y1 = regime1[1]
 
-        xmat2 = regime2[0]
-        y2 = regime2[1]
+        X2 = regime2[0]
+        Y2 = regime2[1]
 
         reg1 = linear_model.LinearRegression()
         reg2 = linear_model.LinearRegression()
 
-        reg1.fit(xmat1, y1)
-        reg2.fit(xmat2,  y2)
+        model1 = reg1.fit(X1, Y1)
+        model2 = reg2.fit(X2,  Y2)
+
 
         eq1 = [reg1.intercept_, reg1.coef_[0], reg1.coef_[1]]
         eq1 = [round(x,3) for x in eq1]
         eq2 = [reg2.intercept_, reg2.coef_[0], reg2.coef_[1]]
         eq2 = [round(x,3) for x in eq2]
 
-    return eq1, eq2 
+        x1 = [i[0] for i in X1]
+        y1 = [i[1] for i in X1]
+        z1 = Y1
 
+        x_pred = np.linspace(0, 6.5, 30)   # range of porosity values
+        y_pred = np.linspace(50, 250, 30)  # range of brittleness values
+        xx_pred, yy_pred = np.meshgrid(x_pred, y_pred)
+        model_viz = np.array([xx_pred.flatten(), yy_pred.flatten()]).T
+
+        predicted = model1.predict(model_viz)
+
+        plt.style.use('default')
+
+        fig = plt.figure(figsize=(12, 4))
+
+        ax1 = fig.add_subplot(131, projection='3d')
+        ax2 = fig.add_subplot(132, projection='3d')
+        ax3 = fig.add_subplot(133, projection='3d')
+
+        axes = [ax1, ax2, ax3]
+
+        for ax in axes:
+            ax.plot(x1, y1, z1, color='k', zorder=15, linestyle='none', marker='o', alpha=0.5)
+            ax.scatter(xx_pred.flatten(), yy_pred.flatten(), predicted, facecolor=(0,0,0,0), s=20, edgecolor='#70b3f0')
+            ax.set_xlabel('Tank water level (m)', fontsize=12)
+            ax.set_ylabel('Water demand (m^3/s)', fontsize=12)
+            ax.set_zlabel('Pump flowrate (m^3/s)', fontsize=12)
+            #  ax.locator_params(nbins=4, axis='x')
+            #  ax.locator_params(nbins=5, axis='x')
+
+
+        ax1.view_init(elev=28, azim=120)
+        ax2.view_init(elev=4, azim=114)
+        ax3.view_init(elev=60, azim=165)
+
+
+        fig.tight_layout()
+        plt.show()
+    return eq1, eq2
+
+regress()
